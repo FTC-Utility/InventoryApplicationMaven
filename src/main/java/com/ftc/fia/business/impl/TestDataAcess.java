3ac29af02c361f335804b8c255451de59ab4a0c2
Zelalem Belay <zelalem.belay1@gmail.com>
2016-12-27 11:36:44 -0500

    Config Completed.

---
+++ src/main/java/com/ftc/fia/business/impl/TestDataAcess.java
@@ -0,0 +1,47 @@
+package com.ftc.fia.business.impl;
+
+import com.ftc.fia.business.ITestDataAccess;
+import com.ftc.fia.domain.TestMe;
+import com.ftc.fia.repository.ITestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Created by Zelalem Belay on 12/23/2016.
+ */
+
+@Service
+@Transactional
+public class TestDataAcess implements ITestDataAccess
+{
+    @Autowired
+    ITestRepository iTestRepository;
+
+    @Override
+    public TestMe getTestById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<TestMe> getTests() {
+        return iTestRepository.findAll();
+    }
+
+    @Override
+    public void createTest(TestMe testMe) {
+        iTestRepository.save(testMe);
+    }
+
+    @Override
+    public void deleteTest(TestMe testMe) {
+
+    }
+
+    @Override
+    public void updateTest(TestMe testMe) {
+
+    }
+}
